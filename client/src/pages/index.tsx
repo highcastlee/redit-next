509dc18487718ea0b79711e838e0945e097e0099
highcastlee <highcastlee@naver.com>
2022-09-30 12:02:50 +0900

    add : infinity scroll

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -9,6 +9,7 @@ import type { NextPage } from 'next';
 import axios from 'axios';
 import { useAuthState } from '../context/auth';
 import useSWR from 'swr';
+import PostCard from '../components/PostCard';
 
 const Home: NextPage = () => {
   const { authenticated } = useAuthState();
@@ -17,21 +18,62 @@ const Home: NextPage = () => {
     return await axios.get(url).then((res) => res.data);
   };
 
+  const getKey = (pageIndex: number, prevPageData: Post[]) => {
+    if (prevPageData && !prevPageData.length) return null;
+    return `/posts?page=${pageIndex}`;
+  };
+
+  const {
+    data,
+    error,
+    size: page,
+    setSize: setPage,
+    isValidating,
+    mutate,
+  } = useSWRInfinite<Post[]>(getKey);
+
+  const isInitialLoading = !data && !error;
+  const posts: Post[] = data ? ([] as Post[]).concat(...data) : [];
+
   const address = 'http://localhost:4000/api/subs/sub/topSubs';
   const { data: topSubs } = useSWR<Sub[]>(address, fetcher);
-  console.log(topSubs);
+
+  const [observedPost, setObservedPost] = useState('');
+
+  useEffect(() => {
+    if (!posts || posts.length === 0) return;
+    const id = posts[posts.length - 1].identifier;
+    if (id !== observedPost) {
+      setObservedPost(id);
+      observeElement(document.getElementById(id));
+    }
+  }, [posts]);
+
+  const observeElement = (element: HTMLElement | null) => {
+    if (!element) return;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting === true) {
+          console.log('마지막 포스트에 도착');
+          setPage(page + 1);
+          observer.unobserve(element);
+        }
+      },
+      { threshold: 1 }
+    );
+    observer.observe(element);
+  };
+
   return (
     <div className="flex max-w-5xl px-4 pt-5 mx-auto">
       {/* 포스트 리스트 */}
       <div className="w-full md:mr-3 md:w-8/12">
-        {/* {isInitialLoading && <p className="text-lg text-center">로딩중입니다...</p>}
-        {posts?.map(post => (
-          <PostCard
-            key={post.identifier}
-            post={post}
-            mutate={mutate}
-          />
-        ))} */}
+        {isInitialLoading && (
+          <p className="text-lg text-center">로딩중입니다...</p>
+        )}
+        {posts?.map((post) => (
+          <PostCard key={post.identifier} post={post} mutate={mutate} />
+        ))}
       </div>
 
       {/* 사이드바 */}
